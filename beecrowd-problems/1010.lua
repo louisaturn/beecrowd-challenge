70dc08f456f41a7d729077dc249220e8996e461e
Louise <louisesaturnino@gmail.com>
2022-12-27 20:52:25 -0300

    1010 solved

--- beecrowd-problems/1010.lua
+++ beecrowd-problems/1010.lua
@@ -1,1 +1,13 @@
 -- Solution of: https://www.beecrowd.com.br/judge/problems/view/1010 Language: Lua
+
+function calculate(unit, price)
+    return unit * price
+end
+
+function main()
+    code1, unit1, price1 = io.read("*n", "*n", "*n")
+    code2, unit2, price2 = io.read("*n", "*n", "*n")
+    return calculate(unit1, price1) + calculate(unit2, price2)
+end
+
+print("VALOR A PAGAR: R$ " .. string.format("%.2f", main()))
